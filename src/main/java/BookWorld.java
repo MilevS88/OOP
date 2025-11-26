97453dcc22b108942e573af6d903f315f5f30fa3
Milev.Sersei <domovenak2425@mail.ru>
2025-11-26 21:42:51 +0300

    Внесение изменений

--- src/main/java/BookWorld.java
+++ src/main/java/BookWorld.java
@@ -2,14 +2,17 @@ import classes.Book;
 
 public class BookWorld {
     public static void main(String[] args) {
+        // Создаем первый объект книги
+        Book book1 = new Book("Анна Каренина", "Лев Толстой", 864);
 
-        Book book1 = new Book();
-        book1.setTitle("Vii");
-        book1.setAuthor("Gogol");
-        book1.setPages(100);
+        // Создаем второй объект книги
+        Book book2 = new Book("Идиот", "Фёдор Достоевский", 576);
 
+        // Выводим информацию о книгах
+        System.out.println("\nКнига №1:");
+        book1.displayInfo();
 
-
-
+        System.out.println("\nКнига №2:");
+        book2.displayInfo();
     }
 }
--- src/main/java/classes/Book.java
+++ src/main/java/classes/Book.java
@@ -1,27 +1,31 @@
 package classes;
 
 public class Book {
-
+    // Поля класса
     private String title;
     private String author;
     private int pages;
 
-    public void displayInfo(String title, String authors, int pages) {
-    }
-
-    public void setTitle(String title) {
+    public Book(String title, String author, int pages) {
         this.title = title;
+        this.setAuthor(author); // используем сеттер для проверки
+        this.pages = pages;
+    }
+    public String getAuthor() {
+        return author;
     }
 
     public void setAuthor(String author) {
-        if (author == "Gogol") {
-            this.author = "Yanovskii";
+        if ("Лев Толстой".equalsIgnoreCase(author)) {
+            this.author = "Александр Пушкин"; // Меняем автора, если указали Толстого
         } else {
             this.author = author;
         }
     }
 
-    public void setPages(int pages) {
-        this.pages = pages;
+    public void displayInfo() {
+        System.out.println("Название книги: " + title);
+        System.out.println("Автор: " + author);
+        System.out.println("Количество страниц: " + pages);
     }
 }
